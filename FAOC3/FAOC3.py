749d7a927ab56ff840de29a95d0792169c926d6e
cubicactivity <stojmanovski355@gmail.com>
2025-12-15 00:11:57 +0100

    commit

--- FAOC3/FAOC3.py
+++ FAOC3/FAOC3.py
@@ -1,3 +1,24 @@
+# Week 3: The Gossip Protocol,
+# The FINKI Discord regulars are spreading rumors about the upcoming exam results.
+#
+# The rumor spreads via Direct Messages (DMs). If User A DMs User B, they share all the information they know. This is transitive: if A talks to B, and B talks to C, then A, B, and C all know the same rumors.
+#
+# We have a log of all DMs that happened today. Each user is identified by a unique ID.
+#
+# Input Format:
+# A single line containing a list of DM pairs separated by commas. Each pair is separated by a hyphen.
+# Example: "1-2,2-3,5-6" means User 1 talked to User 2, User 2 talked to User 3, and User 5 talked to User 6.
+#
+# Your Task:
+# Identify the separate groups of students sharing rumors. Return the size of the largest group of students who all share the same rumor.
+#
+# Example Input:
+# 1-2,2-3,4-5
+#
+# Explanation:
+# Group 1: {1, 2, 3} (Size 3)
+# Group 2: {4, 5} (Size 2)
+# Result: 3
 def dfs(node, graph, visited):
     stack = [node]
     count = 0
